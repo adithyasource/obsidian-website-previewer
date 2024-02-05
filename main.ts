80c89129945ba203f94c7dbdfbc9e7ba0bf3fe8c
adithyasource <me@adithya.zip>
2024-02-06 03:16:52 +0530

    Works Almost Flawlessly + Width and Height Taken Into Account

--- main.ts
+++ main.ts
@@ -10,157 +10,194 @@ import path from "path";
 
 export default class MyPlugin extends Plugin {
 	async onload() {
-		const activeFile = this.app.workspace.getActiveFile();
+		setTimeout(() => {
+			const activeFile = this.app.workspace.getActiveFile();
 
-		let activePath = this.app.vault.getResourcePath(activeFile);
+			let activePath = this.app.vault.getResourcePath(activeFile);
 
-		activePath = activePath.split("/");
+			activePath = activePath.split("/");
 
-		activePath.pop();
+			activePath.pop();
 
-		activePath = activePath.join("/");
+			activePath = activePath.join("/");
 
-		const vaultLocation = this.app.vault.adapter.basePath;
+			const vaultLocation = this.app.vault.adapter.basePath;
 
-		function replaceWebView() {
-			let replacedCount = 0;
+			async function replaceWebView() {
+				let replacedCount = 0;
+				let missingDownloads = 0;
 
-			document.querySelectorAll("webview").forEach(async (e) => {
-				const link = e.getAttribute("src");
+				document.querySelectorAll("webview").forEach(async (e) => {
+					const link = e.getAttribute("src");
 
-				if (link != null) {
-					let imageAlreadyExists = false;
+					let style =
+						e.parentNode.parentNode.parentNode.getAttribute(
+							"style"
+						);
 
-					let imageFileName =
-						link
-							.replaceAll(".", "-")
-							.replaceAll("/", "-")
-							.replaceAll(":", "-") + ".jpg";
+					let widthRegex = /width: (\d+)px;/;
+					let heightRegex = /height: (\d+)px;/;
 
-					let files = await fs.readdir(path.join(vaultLocation, "-"));
+					let width = parseInt(style.match(widthRegex)[1]);
+					let height = parseInt(style.match(heightRegex)[1]);
 
-					files.forEach((file) => {
-						if (file == imageFileName) {
-							imageAlreadyExists = true;
+					if (link != null) {
+						// request(link).then(async (res) => {
+						// 	const ogImageRegex =
+						// 		/<meta\s+property="og:image"\s+content="([^"]+)"\s*\/?>/i;
+						// 	const match = res.match(ogImageRegex);
+						// 	if (match && match.length >= 2) {
+						// 		const img = new Image();
+
+						// 		if (match[1].charAt(0) == "/") {
+						// 			img.src = link + match[1];
+						// 		} else {
+						// 			img.src = match[1];
+						// 		}
+						// 		e.replaceWith(img);
+						// 		replacedCount += 1;
+						// } else {
+						//  no og image found
+
+						let imageAlreadyExists = false;
+
+						let imageFileName =
+							link
+								.replaceAll(".", "-")
+								.replaceAll("/", "-")
+								.replaceAll(":", "-")
+								.replaceAll("#", "-") +
+							width +
+							height +
+							".jpg";
+
+						let files = await fs.readdir(
+							path.join(vaultLocation, "-")
+						);
+
+						files.forEach((file) => {
+							if (file == imageFileName) {
+								imageAlreadyExists = true;
+							}
+						});
+
+						if (imageAlreadyExists) {
+							console.log("img alr exist");
+							const img = new Image();
+
+							let imgPath = activePath + "/-/" + imageFileName;
+
+							img.src = imgPath;
+							console.log(imgPath);
+							e.replaceWith(img);
+
+							console.log(img);
+						} else {
+							// ? image not downloaded
+							missingDownloads += 1;
 						}
-					});
-
-					console.error("up" + imageAlreadyExists);
-
-					if (imageAlreadyExists) {
-						console.log("img alr exist");
-						const img = new Image();
-						let imgPath = activePath + "/-/" + imageFileName;
-
-						img.src = imgPath;
-						console.log(imgPath);
-						e.replaceWith(img);
-
-						console.log(img);
-					} else {
-						console.log("gotta download boss");
+						// 	}
+						// });
 					}
+				});
 
-					// request(link).then(async (res) => {
-					// 	const ogImageRegex =
-					// 		/<meta\s+property="og:image"\s+content="([^"]+)"\s*\/?>/i;
-					// 	const match = res.match(ogImageRegex);
-					// 	if (match && match.length >= 2) {
-					// 		const img = new Image();
-					// 		img.src = match[1];
-					// 		e.replaceWith(img);
-					// 		replacedCount += 1;
-					// 	} else {
-					// 		console.log("no og:image");
-					// 	}
-					// });
-				}
-			});
+				// new Notice("images not downloaded: " + missingDownloads);
 
-			// ! fix this later
-
-			if (replacedCount != 0) {
-				const notice = `replaced ${replacedCount} ${
-					replacedCount == 1 ? "webview" : "webviews"
-				} if og:image was found`;
-
-				new Notice(notice);
-			} else {
-				new Notice("no og:images found");
+				// ! fix this later
 			}
-		}
 
-		function downloadImages() {
-			document.querySelectorAll("webview").forEach(async (e) => {
-				const link = e.getAttribute("src");
+			function downloadImages() {
+				document.querySelectorAll("webview").forEach(async (e) => {
+					const link = e.getAttribute("src");
 
-				if (link != null) {
-					let imageFileName =
-						link
-							.replaceAll(".", "-")
-							.replaceAll("/", "-")
-							.replaceAll(":", "-") + ".jpg";
+					let style =
+						e.parentNode.parentNode.parentNode.getAttribute(
+							"style"
+						);
 
-					const outputPath = path.join(
-						vaultLocation,
-						"-",
-						imageFileName
-					);
-					fetch(
-						"http://127.0.0.1:5002/api?" +
-							new URLSearchParams({
-								url: link,
-							})
-					).then((res) => {
-						res.json().then((jsonres) => {
-							const buffer: Buffer = Buffer.from(jsonres.img);
-							Jimp.read(buffer, (err: Error, image: Jimp) => {
-								if (err) {
-									console.error(err);
-								} else {
-									image.write(outputPath, (err: Error) => {
-										if (err) {
-											console.error(err);
-										} else {
-											console.log(
-												"Image saved successfully"
-											);
-										}
-									});
-								}
+					let widthRegex = /width: (\d+)px;/;
+					let heightRegex = /height: (\d+)px;/;
+
+					let width = parseInt(style.match(widthRegex)[1]);
+					let height = parseInt(style.match(heightRegex)[1]);
+
+					if (link != null) {
+						let imageFileName =
+							link
+								.replaceAll(".", "-")
+								.replaceAll("/", "-")
+								.replaceAll(":", "-")
+								.replaceAll("#", "-") +
+							width +
+							height +
+							".jpg";
+
+						const outputPath = path.join(
+							vaultLocation,
+							"-",
+							imageFileName
+						);
+						fetch(
+							"http://127.0.0.1:5002/api?" +
+								new URLSearchParams({
+									url: link,
+									width: width,
+									height: height,
+								})
+						).then((res) => {
+							res.json().then((jsonres) => {
+								const buffer: Buffer = Buffer.from(jsonres.img);
+								Jimp.read(buffer, (err: Error, image: Jimp) => {
+									if (err) {
+										console.error(err);
+									} else {
+										image.write(
+											outputPath,
+											(err: Error) => {
+												if (err) {
+													console.error(err);
+												} else {
+													console.log(
+														"Image saved successfully"
+													);
+												}
+											}
+										);
+									}
+								});
 							});
 						});
-					});
+					}
+				});
+			}
+
+			document.addEventListener("keyup", async (e) => {
+				if (e.key == "r" && e.ctrlKey && e.altKey) {
+					replaceWebView();
+					console.log("res");
 				}
 			});
-		}
 
-		document.addEventListener("keyup", async (e) => {
-			if (e.key == "s" && e.ctrlKey) {
-				replaceWebView();
-				console.log("res");
-			}
-		});
+			document.addEventListener("keyup", async (e) => {
+				if (e.key == "d" && e.ctrlKey && e.altKey) {
+					console.log("donloading");
+					downloadImages();
+				}
+			});
 
-		document.addEventListener("keyup", async (e) => {
-			if (e.key == "d" && e.ctrlKey) {
-				console.log("donloading");
-				downloadImages();
-			}
-		});
+			this.registerEvent(
+				this.app.workspace.on("file-open", () => {
+					setTimeout(() => {
+						replaceWebView();
+					}, 2000);
+				})
+			);
 
-		document.addEventListener("paste", () => {
-			setTimeout(() => {
-				replaceWebView();
-			}, 200);
-		});
-
-		this.registerEvent(
-			this.app.workspace.on("file-open", () => {
-				setTimeout(() => {
+			try {
+				setInterval(() => {
 					replaceWebView();
-				}, 200);
-			})
-		);
+				}, 2000);
+			} catch (error) {}
+		}, 1000);
 	}
 }
